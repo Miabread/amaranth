d99bd621295bec425fd8c4c5d15228c8aed0ce2e
Butter Cat <ButteredCats@protonmail.com>
2026-03-26 12:46:33 -0400

    Create a basic page that demos templating and serving static files that can be built on later

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Things under /static are served directly without needing anything here
+# Every time you make a change to any files being served you gotta restart the webserver too
 
 @app.route('/')
 def render_base():
     # This works relative to the templates folder by default
-    return render_template("extend.html")
+    return render_template("home.html")
 
-@app.route("/<name>")
+@app.route("/user/<name>")
 def welcome(name):
-    return render_template("extend.html", name=name)
+    return render_template("user.html", name=name)
 
 # This whines about "This is a development server. Do not use it in a production deployment. Use a production WSGI server instead."
 # but that's something to fix in the future. It just requires a different way of starting the sever using some other dependency
